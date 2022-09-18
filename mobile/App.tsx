d1e231954ad22752dfe54113e0bd1888b3b305e1
Johannes Alves <johannesprofissional@gmail.com>
2022-09-18 00:10:47 -0300

    test

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,6 +9,7 @@ import {
 
 import { Home } from "./src/screens/Home";
 import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
             />
-            {fontsLoaded ? <Home /> : }
+            {fontsLoaded ? <Home /> : <Loading /> }
         </Background>
     );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import { styles } from "./styles";
 
 export function Home() {
-    return <View style={styles.container}></View>;
+    return <View style={styles.container}>
+    </View>;
 }
